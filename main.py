aa4574e9a9d39cb8a60878ea9c3f35aaf2268e1a
StaBOL <istasyan07@gmail.com>
2025-02-27 11:30:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def print_string(word):
 def count_vowels(word):
     vowels = "аеёиоуыэюя"
     return sum(1 for letter in word if letter in vowels)
+
+def onefunc(word):
+    return word[::-1]
+
+def removeduplicates(word):
+    unique_chars = ""
+    for char in word:
+        if char not in unique_chars:
+            unique_chars += char
---
+++ testii.py
@@ -0,0 +1,12 @@
+import unittest
+import main
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(main.print_string("bab"), "bab")  # add assertion here
+    def test_something2(self):
+            self.assertEqual(main.count_vowels("сом"), 1)
+    def test_something3(self):
+            self.assertEqual(main.onefunc("hello"),"olleh")
+    def test_something4(self):
+                self.assertEqual(main.removeduplicates("сонн"),"сон")
